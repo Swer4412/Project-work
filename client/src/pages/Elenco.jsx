38e825f941ca832473bdd8c2c51bdd3a9c8c50c6
swer4412 <swer4412@gmail.com>
2023-05-08 12:12:39 +0200

    a

--- client/src/pages/Elenco.jsx
+++ client/src/pages/Elenco.jsx
@@ -4,26 +4,26 @@ const Elenco = () => {
 
     const [data, setData] = useState(undefined);
 
+    //Pendo i dati dal backend appena viene caricata la pagina
     useEffect(()=> {
-        fetch("localhost:5000/users")
+        fetch("http://localhost:5000/users")
         .then((response) => response.json())
         .then((data) => setData(data))
     }, [])
 
     return (
-        <main>
+        <>
             {!data ? 
-            <p>Loading...</p>
+            <p>Caricamento...</p>
             : 
             data.map((item)=> (
-                <p>{item.title}</p>,
-                <img src={item.imageUrl}></img>,
-                <p>{item.description}</p>,
-                <p>{item.watchDate}</p>,
-                <p>{item.userComment}</p>
+                <div>
+                <p>{item.title}</p>
+                <p>{item.description}</p>
+                </div>
             ))    
         }
-        </main>
+        </>
     );
 };
 
