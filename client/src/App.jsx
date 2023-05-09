0340fbdad557068ea9c78b445ad97f095a1c28a3
Swer4412 <Swer4412@gmail.com>
2023-05-09 11:29:43 +0200

    aggiunto tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} fr
 import NavBar from "./layouts/NavBar"
 import Elenco from './pages/Elenco'
 import Inserimento from './pages/Inserimento'
+import NotFound from './pages/NotFound'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<NavBar/>}>
       <Route index element={<Elenco/>}/>
       <Route path="inserimento" element={<Inserimento/>}/>
+
+      <Route path="*" element={<NotFound/>}/>
     </Route>
   )
 )
--- client/src/layouts/NavBar.jsx
+++ client/src/layouts/NavBar.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <>
-        <header>
-            <nav>
-                <h1>MySeries</h1>
-                <NavLink to="/">MyMedia</NavLink>
-                <NavLink to="inserimento">Inserisci</NavLink>
-            </nav>
-        </header>
-        <main>
-            <Outlet/>
-        </main>
-        </>
-    );
+  return (
+    <>
+      <header className="bg-gray-900 text-white sticky top-0 z-50">
+        <nav className="flex justify-between items-center p-4">
+          <h1 className="text-2xl font-bold">MySeries</h1>
+          <div>
+            <NavLink to="/" className="px-4 py-2 rounded hover:bg-gray-700">
+              MyMedia
+            </NavLink>
+            <NavLink
+              to="inserimento"
+              className="px-4 py-2 rounded hover:bg-gray-700"
+            >
+              Inserisci
+            </NavLink>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default NavBar;
